69156c74f85e604bac104d182304cb108ccebc07
lyman wong <lymanwong@gmail.com>
2015-07-13 22:09:32 -0700

    refactored

--- algorithms/highest_and_lowest/highest_and_lowest.js
+++ algorithms/highest_and_lowest/highest_and_lowest.js
@@ -1,11 +1,19 @@
+// function high_and_low(string){
+//   var num = [];
+//   arr = string.split(" ");
+//   for(var i = 0; i<arr.length; i++){
+//     num.push(parseInt(arr[i],10));
+//   }
+//   return((Math.max.apply(null,num).toString()) + " " + (Math.min.apply(null,num).toString()));
+// }
+
+// refactored
+
 function high_and_low(string){
-  var num = [];
   arr = string.split(" ");
-  for(var i = 0; i<arr.length; i++){
-    num.push(parseInt(arr[i],10));
-  }
-  return((Math.max.apply(null,num).toString()) + " " + (Math.min.apply(null,num).toString()));
+  return((Math.max.apply(null,arr).toString()) + " " + (Math.min.apply(null,arr).toString()));
 }
+
 
 print(high_and_low("1 9 3 4 -5") === "9 -5");
 print(high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6") === "542 -214");
