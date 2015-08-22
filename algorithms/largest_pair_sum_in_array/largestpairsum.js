8e6631cb310ba6f5213199d81b54e9a622626aee
lyman wong <lymanwong@gmail.com>
2015-08-21 18:12:38 -0700

    completed

--- algorithms/largest_pair_sum_in_array/largestpairsum.js
+++ algorithms/largest_pair_sum_in_array/largestpairsum.js
@@ -9,3 +9,13 @@ For example
 
 Input array contains minimum two elements and every element is a number.
 */
+
+function largestpairsum(numbers) {
+  var sorted_array = numbers.sort(function(a,b){
+    return a-b;
+  });
+  return (sorted_array[sorted_array.length-1]) + (sorted_array[sorted_array.length-2])
+}
+
+debug(largestpairsum([10,14,2,23,19]));
+debug(largestpairsum([99,2,2,23,19]));
