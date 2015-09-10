b66fd6a82b763080a4bc98e214fd07a891bb5450
lyman wong <lymanwong@gmail.com>
2015-09-09 22:02:14 -0700

    still working on it

--- algorithms/insert_dashes/insert_dashes.js
+++ algorithms/insert_dashes/insert_dashes.js
@@ -3,5 +3,20 @@ Write a function insertDash(num) that will insert dashes ('-') between each two 
 */
 
 function insertDash(num) {
-
+var numString = num.toString().split('');
+var collection = [];
+for(var i=0; i < numString.length; i+=1){
+  if(numString[i]%2===0){
+      collection.push(numString[i]);
+    }else{
+      collection.push("-");
+        collection.push(numString[i]);
+    }
 }
+  debug(collection.join(''));
+}
+
+
+insertDash(454793); //'4547-9-3'
+//insertDash(123456); //'123456'
+//insertDash(1003567); //'1003-567'
