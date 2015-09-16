dfe34d426aec3c0a2df08ba962a628c3114c1f0b
lyman wong <lymanwong@gmail.com>
2015-09-15 23:11:09 -0700

    initial commit

---
+++ algorithms/sum_all_arrays/sum_all_arrays.js
@@ -0,0 +1,15 @@
+/*
+You are given an array of values.
+
+Sum every number value in the array, and any nested arrays (to any depth).
+
+Ignore all other types of values.
+*/
+
+function arraySum(arr){
+
+}
+
+arraySum([1,2]) //3
+arraySum([1,2,3]) //6
+arraySum([1,2,[1,2]]) //6
