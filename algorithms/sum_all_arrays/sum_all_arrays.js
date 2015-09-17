8734e5e175098d7349007266f6c723737ce3b358
lyman wong <lymanwong@gmail.com>
2015-09-16 21:40:07 -0700

    finished

--- algorithms/sum_all_arrays/sum_all_arrays.js
+++ algorithms/sum_all_arrays/sum_all_arrays.js
@@ -7,9 +7,20 @@ Ignore all other types of values.
 */
 
 function arraySum(arr){
-
+var sum = 0;
+for(var i=0; i<arr.length; i++){
+  if(typeof arr[i] === 'number'){
+    sum += arr[i];
+    }
+  else {
+      for(var j=0, len=arr[i].length; j<len; j++){
+        sum += arr[j];
+      }
+  }
+}
+return sum;
 }
 
-arraySum([1,2]) //3
-arraySum([1,2,3]) //6
-arraySum([1,2,[1,2]]) //6
+//debug(arraySum([1,2])); //3
+//debug(arraySum([1,2,3])); //6
+debug(arraySum([1,2,[1,2]])); //6
