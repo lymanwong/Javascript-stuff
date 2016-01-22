f8940689ff1756f8edc93ab2a42f39c84df16182
lyman wong <lymanwong@gmail.com>
2016-01-21 18:05:37 -0800

    done

---
+++ callback_practice/filter_autobots/index.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+function getAutobots (filepath, done) {
+  fs.readFile(filepath, function (err, autobots){
+    if(err) return done(err);
+
+    fs.readFile('autobots.dictionary', function(err, dict){
+      if (err) return done(err);
+
+      compareAutobots(autobots, dict);
+    })
+  })
+
+  function compareAutobots (autobots, dict) {
+    dict = dict.toString().split('\n');
+    autobots = autobots.toString().split('\n').filter(function(autobot){
+      return dict.indexOf(autobot) !== -1;
+    })
+    done(null, autobots);
+  }
+}
+
+
+getAutobots('autobots.txt', function(err, autobots){
+  console.log(autobots);
+})
