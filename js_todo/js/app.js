44e8cbe2cd0470b263fa069208a971b0b005fd32
lyman wong <lymanwong@gmail.com>
2016-02-07 14:39:20 -0800

    select elements

--- js_todo/js/app.js
+++ js_todo/js/app.js
@@ -2,7 +2,14 @@
 
 //Solution: Add interactivity so the user can manage daily tasks
 
+var taskInput = document.getElementById("new-task"); //new-task
+var addButton = document.getElementsByTagName("button")[0]; //first button
+var incompleteTasksHolder = document.getElementById("incomplete-tasks"); //incomplete-task
+var completedTasksHolder = document.getElementById("completed-tasks"); //completed-tasks
+
 //Add a new task
+var addTask = function(){
+
   //When the button is pressed
   //Create a new list item with the text from #new-task:
     //input (checkbox)
@@ -11,9 +18,12 @@
     //button.edit
     //button.delete
     //Each elements, needs modified and appended
+}
 
 
 //Edit an exisiting task
+var editTask = function(){
+
   //When the Edit botton is pressed
     //if the class of the parent is .editMode
       //Switch from .editMode
@@ -24,16 +34,26 @@
 
   //Toggle .editMode on the parent
 
+}
 //Delete an existing task
+var deleteTask = function(){
+
   //When the Delete button is pressed
     //Remove the parent list item from the ul
 
+}
 
 //Mark a task as complete
+var taskCompleted = function(){
+
   //When the Checkbox is checked
     //Append the task list item to the #completed-tasks
 
+}
 
 //Mark a task as incomplete
+var taskIncomplete = function() {
+
   //When the checkbox is unchecked
     //Append the task list item to the #incomplete-tasks
+}
