604cd57726a00fa35c267b5e473e314e548cebeb
lyman wong <lymanwong@gmail.com>
2016-03-07 12:49:44 -0800

    create js file

--- ajax_practice/comicbookstore/js/comicbookstore.js
+++ ajax_practice/comicbookstore/js/comicbookstore.js
@@ -1,1 +1,38 @@
-comicbookstore.js
+var xmlHttp = createXmlHttpRequestObject();
+
+function createXmlHttpRequestObject() {
+  var xmlHttp;
+
+  //make ajax to work for IE users
+  if(window.ActiveXObject) {
+    try {
+      //set object to the variable in IE
+      xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    } catch(e) {
+      xmlHttp = false;
+    }
+  } else {
+    try {
+      xmlHttp = new XMLHttpRequest();
+    } catch(e) {
+      xmlHttp = false;
+    }
+  }
+}
+
+  if(!xmlHttp)
+    alert("Can't create that object bro");
+  else
+    return xmlHttp;
+
+function process(){
+  if(xmlHttp.readyState==0 || xmlHttp.readyState==4) {
+    hero = encodeURIComponent(document.getElementById("userInput").value);
+    xmlHttp.open("GET", "comicbookstore.php?hero=" + hero, true);
+    //handle server response
+    xmlHttp.onreadystatechange = handleServerResponse;
+    xmlHttp.send(null);
+  } else {
+    setTimeout('process()', 1000);
+  }
+}
