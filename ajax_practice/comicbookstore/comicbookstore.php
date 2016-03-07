1bedda731bc9a546b6e449ad6f18345ad27a398c
lyman wong <lymanwong@gmail.com>
2016-03-07 14:04:10 -0800

    fixed bug

--- ajax_practice/comicbookstore/comicbookstore.php
+++ ajax_practice/comicbookstore/comicbookstore.php
@@ -3,13 +3,13 @@ header('Content-Type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
 
 echo '<response>';
-  $hero = strtolower($_GET['hero']);
-  $heroArray = array('captain america', 'spiderman', 'superman', 'batman', 'aquaman', 'robin', 'flash');
+  $hero = ucwords($_GET['hero']);
+  $heroArray = array('Captain America', 'Spiderman', 'Superman', 'Batman', 'Aquaman', 'Robin', 'Flash');
   if(in_array($hero, $heroArray))
-    echo 'We do have '.$hero.'comics!';
+    echo 'We do have '.$hero.' comics!';
   elseif($hero == '')
     echo 'Enter a hero name.';
   else
-    echo 'Sorry, we do not have '.$hero.'comics.';
+    echo 'Sorry, we do not have '.$hero.' comics.';
 echo '</response>';
 ?>
--- ajax_practice/comicbookstore/js/comicbookstore.js
+++ ajax_practice/comicbookstore/js/comicbookstore.js
@@ -19,18 +19,18 @@ function createXmlHttpRequestObject() {
       xmlHttp = false;
     }
   }
-}
+
 
   if(!xmlHttp)
     alert("Can't create that object bro");
   else
     return xmlHttp;
-
+}
 //create function to communicate with server
 function process(){
   if(xmlHttp.readyState==0 || xmlHttp.readyState==4) {
     hero = encodeURIComponent(document.getElementById("userInput").value);
-    xmlHttp.open("GET", "comicbookstore.php?hero=" + hero, true);
+    xmlHttp.open("GET", "comicbookstore.php?hero=" +hero, true);
     //handle server response
     xmlHttp.onreadystatechange = handleServerResponse;
     xmlHttp.send(null);
@@ -43,11 +43,11 @@ function process(){
 function handleServerResponse(){
   //check xml file for errors
   if(xmlHttp.readyState==4) {
-    if(xmlHttp.readyState==200) { //ok
+    if(xmlHttp.status==200) { //ok
       xmlResponse = xmlHttp.responseXML;
       xmlDocumentElement = xmlResponse.documentElement;
       message = xmlDocumentElement.firstChild.data;
-      document.getElementById("userInput").innerHTML = '<span style="color:red">'+ message + '</span>';
+      document.getElementById("underInput").innerHTML = '<span style="color:blue">'+ message + '</span>';
       setTimeout('process()', 1000);
     } else {
       alert ('Something went wrong :( ');
