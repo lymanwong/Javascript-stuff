6f65c497a3c0e5a98fb2b9cca7898b701a1fe488
lyman@le <lymanwong@gmail.com>
2016-05-31 15:03:58 -0700

    initial solultion;

--- algorithms/math_issues/math_issues.js
+++ algorithms/math_issues/math_issues.js
@@ -8,12 +8,20 @@ Math.floor()
 
 */
 
+
 Math.round = function(number) {
 	var stringNumber = number.toString();
+	var isWholeNumber = Number.isInteger(number);
 	var decimalLocation = stringNumber.indexOf(".");
 	var wholeNumbers = stringNumber.slice(0,decimalLocation);
 	var decimalNumbers = stringNumber.slice(decimalLocation + 1, stringNumber.length);
-	if (decimalNumbers >= 5) {
+	if (isWholeNumber === true) {
+		if (wholeNumbers == 0 && decimalNumbers == 0) {
+			return 0;
+		} else {
+			return number;
+		}
+	} else if (decimalNumbers[0] >= 5){
 		return parseInt(wholeNumbers) + 1;
 	} else { 
 		return parseInt(wholeNumbers); 
@@ -21,21 +29,57 @@ Math.round = function(number) {
 };
 
 Math.ceil = function(number) {
-  return 0; // TODO: fix this
+  	var stringNumber = number.toString();
+  	var isWholeNumber = Number.isInteger(number);
+	var decimalLocation = stringNumber.indexOf(".");
+	var wholeNumbers = stringNumber.slice(0,decimalLocation);
+	var decimalNumbers = stringNumber.slice(decimalLocation + 1, stringNumber.length);
+	if (isWholeNumber === true) {
+		if (wholeNumbers == 0 && decimalNumbers == 0) {
+			return 0;
+		} else {
+			return number;
+		}
+	} else {
+	return parseInt(wholeNumbers) + 1;
+	}
 };
 
 Math.floor = function(number) {
-  return 0; // TODO: fix this
+  	var stringNumber = number.toString();
+  	var isWholeNumber = Number.isInteger(number);
+	var decimalLocation = stringNumber.indexOf(".");
+	var wholeNumbers = stringNumber.slice(0,decimalLocation);
+	var decimalNumbers = stringNumber.slice(decimalLocation + 1, stringNumber.length);
+	if (isWholeNumber === true) {
+		if (wholeNumbers == 0 && decimalNumbers == 0) {
+			return 0;
+		} else {
+			return number;
+		}
+	} else {
+	return parseInt(wholeNumbers);
+	}
 };
 
-debug(Math.round(0.4)); //0
-debug(Math.round(0.5)); //1
-debug(Math.round(0.6)); //1
-debug(Math.round(2.5)); //3
-debug(Math.round(4.4)); //4
+debug(Math.round(7.12)); //7
+debug(Math.round(2.499999)); //2
+// debug(Math.round(0)); //0
+// debug(Math.round(0.4)); //0
+// debug(Math.round(0.5)); //1
+// debug(Math.round(0.6)); //1
+// debug(Math.round(2.5)); //3
+// debug(Math.round(4.4)); //4
+// debug(Math.round(5)); //5
+// debug(Math.round(2.499999)); //3
 
-// Math.ceil(0.4); //1
-// Math.ceil(0.5); //1
+// debug(Math.ceil(0)); //0
+// debug(Math.ceil(5)); //5
+// debug(Math.ceil(0.4)); //1
+// debug(Math.ceil(2.5)); //3
+// debug(Math.ceil(4.5)); //5
 
-// Math.floor(0.4); //0
-// Math.floor(0.5); //0
+// debug(Math.floor(0.4)); //0
+// debug(Math.floor(0.5)); //0
+// debug(Math.floor(0)); //0
+// debug(Math.floor(5)); //5
